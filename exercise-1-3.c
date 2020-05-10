251ac316e58dd57e29f0fd8260e1f5e948492e61
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-11 01:05:34 +0530

    Completed exercise-1-3.c

---
+++ exercise-1-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Temperature Conversion Chart (F -> C)\n"); //Heading
+
+  float fahr, cels;
+  int lower, upper, step; // Lower limit, upper limit and step declaration
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper)
+  {
+    cels = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, cels);
+    fahr += step;
+  }
+
+  return 0;
+}
