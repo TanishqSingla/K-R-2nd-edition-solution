e5c5e94b5bd76be4345c8eb0cdcc4fa8a018d642
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-11 23:49:08 +0530

    Added exercise-1-14.c

---
+++ Exercise-1/exercise-1-14.c
@@ -0,0 +1,9 @@
+/* Since the question didn't specified to include all the character, so I am just considering the alphabets and symbols */
+#include <stdio.h>
+
+int main()
+{
+  int c;
+
+  return 0;
+}
